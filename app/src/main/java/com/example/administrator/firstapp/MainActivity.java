1efd8af096211cbf9dd262ff3c87048af9a8ee27
1051353960 <1051353960@qq.com>
2017-05-29 12:56:09 +0800

    5.29.12.55
    5.29.12.55

--- app/src/main/java/com/example/administrator/firstapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/firstapp/MainActivity.java
@@ -6,13 +6,16 @@ import android.os.Handler;
 import android.renderscript.Element;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
-
+private RadioGroup mRg_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRg_main =(RadioGroup) findViewById(R.id.rg_bottom_tag);
+        mRg_main.check(R.id.rb_common_frame);
 
     }
     }
